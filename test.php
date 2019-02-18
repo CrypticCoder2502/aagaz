a2157865d3d6ddc43eb83fae5d7c86faf1926862
CrypticCoder2502 <43320248+CrypticCoder2502@users.noreply.github.com>
2019-02-18 23:55:37 +0530

    123

---
+++ test.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <head>
+        <link rel="stylesheet" href="animations.css" type="text/css">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Acme|Pacifico|Kosugi|Lato" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lobster|Righteous|Indie+Flower|Courgette" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Acme|Pacifico|Kosugi|Lato|Black+Ops+One|Bubblegum+Sans|Fredericka+the+Great|Limelight|Monoton|Shojumaru" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <link href="lightbox.css" rel="stylesheet">
+  <script src="lightbox.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+    <div class="col-sm-4">
+      <a href="gallery/1.jpeg" data-lightbox="roadtrip"><img src="gallery/1.jpeg" class="img-responsive"></a>
+    </div>
+    <div class="col-sm-4">
+      <a href="gallery/2.jpeg" data-lightbox="roadtrip"><img src="gallery/2.jpeg" class="img-responsive"></a>
+    </div>
+    <div class="col-sm-4">
+      <a href="gallery/3.jpeg" data-lightbox="roadtrip"><img src="gallery/3.jpeg" class="img-responsive"></a>
+    </div> 
+    </div>
+  </div>
+
+
+</body>
+</html>
